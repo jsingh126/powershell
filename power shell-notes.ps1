23f73f034d36ea14716ac7357498a5d7adb8b935
Jitendra Singh Charan <jsingh126@gmail.com>
2022-03-31 18:08:57 +0400

    Update power shell-notes.ps1

--- power shell-notes.ps1
+++ power shell-notes.ps1
@@ -2,8 +2,10 @@
 Install-Module -Name DnsClient-PS
 
 ## Azure vm tags
-## https://stackoverflow.com/questions/58345592/azure-powershell-tagging-vms-from-csv-file
+ https://stackoverflow.com/questions/58345592/azure-powershell-tagging-vms-from-csv-file
 
 ## save error to variable, $r saves the error and $output has the exact output of the error.
 $r = $($output = Test-Connection esx1.mylab.local) 2>&1
 
+# generate csv data
+https://www.convertcsv.com/generate-test-data.htm

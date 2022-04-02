9abd7372a952b2eebb3af8df8cfce6524b7b0bae
Jitendra Singh Charan <jsingh126@gmail.com>
2022-04-02 22:44:23 +0400

    Create DNS-report.ps1

---
+++ DNS-report.ps1
@@ -0,0 +1,28 @@
+$FinalResult = @()
+$input_csv = ""
+
+#importing the csv
+
+ $vms = Import-Csv -path $input_csv
+
+
+foreach ($vm in $vms) {   
+     $tempObj = "" | Select-Object Name,PTR,Arecord,Status,ErrorMessage 
+    try {     
+           #$dnsRecorda = Resolve-DnsName $vm.FQDN  -ErrorAction Stop | Where-Object {$_.Type -eq 'A'}     
+           $dnsRecordp = Resolve-DnsName $vm.Azure_IP  -ErrorAction Stop | Where-Object {$_.Type -eq 'PTR'}     
+           $tempObj.Name = $vm.Azure_IP  
+           $tempObj.Arecord = ($dnsRecorda.IPAddress -join ',')    
+           $tempObj.PTR = ($dnsRecordp.NameHost -join ',')
+          $tempObj.Status = 'OK'    
+          $tempObj.ErrorMessage = ''    }    
+    catch
+     {        $tempObj.Name = $vm.Azure_IP 
+            $tempObj.PTR = ''       
+             $tempObj.Status = 'NOT_OK'       
+             $tempObj.Arecord = ''
+              $tempObj.ErrorMessage = $_.Exception.Message    }   
+               $FinalResult += $tempObj
+            }
+            
+            return $FinalResult | ft
